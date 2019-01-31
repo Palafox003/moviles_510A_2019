7331d5d72bcb5d14a69a434e6488f29b4de87f3a
Palafox003 <israelpalafox@utgz.edu.mx>
2019-01-31 12:10:33 -0600

    Actualización se aplicación con laravel y modelo de base de datos

---
+++ blog/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//----------------------------------------------
+Route::get('palafox', function () {
+    return 'Hello World';
+});
+//---------------------------------------------
+Route::get('user/{nombre?}', function ($nombre="Por favor inserta tu nombre") {
+    return 'User: '.$nombre;
+});
+//------------------------------------------
+Route::get('libros','librosController@index');
